870378764bdeef2626c86b9f18a6ecb9cc443164
Carlo Bortolan <106114526+carlobortolan@users.noreply.github.com>
2025-03-14 14:47:01 +0100

    Update module name (#11)
    * Update crate naming to quantrs, update package structure
    
    * Add Dependabot config

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! # quantrs
+//! A tiny Rust library for options pricing, portfolio optimization, and risk analysis.
+
+pub mod data;
+pub mod fixed_income;
+pub mod options;
+pub mod portfolio;
+pub mod timeseries;
