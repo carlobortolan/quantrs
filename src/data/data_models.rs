2f6393288ce0bb431969b4cfbb202fa824c95493
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2025-07-10 00:07:26 -0400

    fix: allowing for dead code to fix linting checks

--- src/data/data_models.rs
+++ src/data/data_models.rs
@@ -1,5 +1,6 @@
 //! Module holds data models and structs for deserializig from JSON responses.
 
+#[allow(dead_code)]
 pub enum Resolution {
     Intraday,
     Daily,
--- src/data/data_provider/alpha_vantage.rs
+++ src/data/data_provider/alpha_vantage.rs
@@ -1,11 +1,13 @@
 //! Module that provides the Alpha Vantage data provider implementation.
 
+#[allow(dead_code)]
 pub struct AlphaVantageSource {
     base_url: String,
     api_key: String,
 }
 
 impl AlphaVantageSource {
+    #[allow(dead_code)]
     pub fn new(user_key: &str) -> Self {
         AlphaVantageSource {
             base_url: "https://www.alphavantage.co/query".to_string(),
