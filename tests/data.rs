8e9cdb246fa22f0d12eac80a300bf0029463cb40
Carlo Bortolan <106114526+carlobortolan@users.noreply.github.com>
2025-07-11 01:03:55 +0200

    Update tests/data.rs
    Signed-off-by: Carlo Bortolan <106114526+carlobortolan@users.noreply.github.com>

--- tests/data.rs
+++ tests/data.rs
@@ -3,7 +3,7 @@ use quantrs::data::AlphaVantageSource;
 mod data_tests {
     use super::*;
 
-    #[allow(dead_code)]
+    #[test]
     fn test_alpha_vantage() {
         let _source = AlphaVantageSource::new("demo");
     }
