726e54301c107065fade7ec13037d9068953e3c7
carlobortolan <carlobortolan@gmail.com>
2025-08-20 01:23:32 +0200

    Remove out unused future bond imports

--- tests/fixed_income.rs
+++ tests/fixed_income.rs
@@ -2,29 +2,21 @@
 mod tests {
     mod corporate_bond_tests {
         #[test]
-        fn test_corporate_bond_price() {
-            assert!(true, "Corporate bond pricing test not implemented yet");
-        }
+        fn test_corporate_bond_price() {}
     }
 
     mod floating_rate_bond_tests {
         #[test]
-        fn test_floating_rate_bond_price() {
-            assert!(true, "Floating rate bond pricing test not implemented yet");
-        }
+        fn test_floating_rate_bond_price() {}
     }
 
     mod treasury_bond_tests {
         #[test]
-        fn test_treasury_bond_price() {
-            assert!(true, "Treasury bond pricing test not implemented yet");
-        }
+        fn test_treasury_bond_price() {}
     }
 
     mod zero_coupon_bond_tests {
         #[test]
-        fn test_zero_coupon_bond_price() {
-            assert!(true, "Zero coupon bond pricing test not implemented yet");
-        }
+        fn test_zero_coupon_bond_price() {}
     }
 }
